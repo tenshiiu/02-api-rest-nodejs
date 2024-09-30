b5c388f62b118d230b476cccfaf55faf7b5f7406
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-09-30 14:34:25 -0300

    Tipagem no Knex

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,15 @@
+// Um arquivo .d.ts significa: definição de tipos
+
+import { Knex } from "knex";
+
+declare module "knex/types/tables" {
+    export interface Tables {
+        transactions: {
+            id: string
+            title: string
+            amount: number
+            created_at: string
+            session_id?: string
+        }
+    }
+}
